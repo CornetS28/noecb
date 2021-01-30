c89d35c56eef5b7367f6cbbdd5bd6ff9c3995ddb
CornetS28 <corsam28@gmail.com>
2021-01-30 14:13:01 -0500

    Create a global theme

---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+export default {
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#cccccc",
+    },
+
+    font: {
+      fontFamily: "HelveticaNeue",
+      fontStretch: "normal",
+      lineHeight: 1.17,
+      letterSpacing: "0.23px",
+      textAlign: "center",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+};
