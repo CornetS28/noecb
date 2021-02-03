93e247bb94b0470c93f82708974cb39b0e136ae8
CornetS28 <corsam28@gmail.com>
2021-02-03 12:11:54 -0500

    Create landing page (to be continued...)

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
 
-const LandingPage = () => {
+// MUI Stuff
+import Grid from "@material-ui/core/Grid";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// Images
+import LandingImage from "../images/mainHomeImage.png";
+
+const LandingPage = (props) => {
+  const { classes } = props;
   return (
-    <div>
-      <h1>Langing Page</h1>
-    </div>
-  )
-}
+    <Grid container className={classes.landingPageWrapper}>
+      <Grid container>
+        <Grid item sm />
+        <Grid item sm={12} xm={12}>
+          <img
+            src={LandingImage}
+            alt="portrait"
+            className={classes.mainImage}
+          />
+        </Grid>
+        <Grid item sm />
+      </Grid>
+      <Grid sm={12} xs={12} style={{ marginTop: -3 }}>
+        <div className={classes.membersCount}>
+          <span className={classes.membersCountText}>Current Members: </span>
+          <span className={classes.number}> 4,433 </span>
+        </div>
+      </Grid>
+       <Grid sm={12} xs={12} className={classes.newSection}>
+         <Grid sm={8} xm={10} className={classes.subSction}>
+           sajhscd
+         </Grid>
+       </Grid>
+    </Grid>
+  );
+};
 
-export default LandingPage;
+const styles = (theme) => ({
+  landingPageWrapper: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  mainImage: {
+    margin: "auto",
+    width: "100%",
+  },
+
+  membersCount: {
+    lineHeight: 1.5,
+    textAlign: "center",
+    backgroundColor: theme.palette.color.white,
+    fontStyle: theme.palette.font.BebasNeueBold,
+  },
+  membersCountText: {
+    textTransform: "uppercase",
+    fontSize: 30,
+    fontWeight: 900,
+    color: theme.palette.color.darkBlue,
+  },
+  number: {
+    fontSize: 55,
+    fontWeight: 900,
+    color: theme.palette.color.yellow,
+  },
+  newSection: {
+    backgroundColor: theme.palette.color.darkWhite,
+  },
+  subSction: {
+    margin: "50px auto 50px auto",
+    backgroundColor: theme.palette.color.white,
+  },
+});
+export default withStyles(styles)(LandingPage);
