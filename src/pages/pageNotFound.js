43a32da237709770de008eb44926392762beb3da
CornetS28 <corsam28@gmail.com>
2021-02-25 14:19:05 -0500

    Fix responsiveness issue for the 'PageNotFound'

--- src/pages/pageNotFound.js
+++ src/pages/pageNotFound.js
@@ -10,13 +10,13 @@ const PageNotFound = (props) => {
   const { classes } = props;
   return (
     <Grid container className={classes.Page404Wrapper}>
-      <Grid container item sm={9} xs={12} className={classes.Page404Container}>
-        <Grid item sm={7} xs={12}>
-          <div edge="start" className={classes.logoWrapper}>
-            <img src={Dog404} alt="logo" className={classes.logo} />
+      <Grid container item sm={8} xs={12} className={classes.page404Container}>
+        <Grid item xs={12} sm={7} className={classes.page404}>
+          <div edge="start" className={classes.dogContainer}>
+            <img src={Dog404} alt="dog" className={classes.dogImage} />
           </div>
         </Grid>
-        <Grid item sm={5} xs={12}>
+        <Grid item xs={12} sm={5} className={classes.error404Text}>
           <div className={classes.error404}>
             <h1>Oops!</h1>
             <h2 className={classes.error404TextYellow}>It looks like our</h2>
@@ -39,27 +39,43 @@ const styles = (theme) => ({
   Page404Wrapper: {
     backgroundColor: theme.palette.primary.main,
   },
-  Page404Container: {
-    margin: "auto",
-    [theme.breakpoints.down(1522)]: {
-      margin: "80px  auto 80px auto",
+  page404Container: {
+    margin: "60px auto 50px auto",
+    [theme.breakpoints.down(1330)]: {
+      margin: "auto auto auto auto",
     },
-    [theme.breakpoints.down(1121)]: {
-      margin: "85px  auto 85px auto",
+  },
+  page404: {
+    margin: "10px auto auto auto",
+    [theme.breakpoints.down(1330)]: {
+      margin: "10% auto 20% auto",
     },
-    [theme.breakpoints.down(1094)]: {
-      margin: "90px  auto 90px auto",
+    [theme.breakpoints.down(991)]: {
+      marginTop: "20%",
     },
-    [theme.breakpoints.down(1067)]: {
-      margin: "100px  auto 100px auto",
-    },
-    [theme.breakpoints.down(850)]: {
-      margin: "120px  auto 120px auto",
+    [theme.breakpoints.down(783)]: {
+      margin: "30% auto 20% auto",
     },
     [theme.breakpoints.down(600)]: {
       margin: "auto",
     },
   },
+  error404Text: {
+    margin: "10px auto auto auto",
+    [theme.breakpoints.down(1330)]: {
+      margin: "10% auto 20% auto",
+    },
+    [theme.breakpoints.down(990)]: {
+      margin: "15% auto 20% auto",
+    },
+    [theme.breakpoints.down(783)]: {
+      marginBottom: "30%",
+    },
+    [theme.breakpoints.down(600)]: {
+      margin: "auto auto 6% auto",
+    },
+  },
+  dogContainer: {},
   error404: {
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.color.red,
@@ -70,28 +86,33 @@ const styles = (theme) => ({
     paddingTop: 20,
     paddingBottom: 20,
     fontFamily: theme.palette.font.HelveticaNeue,
-    [theme.breakpoints.down(1131)]: {
+    [theme.breakpoints.down(1428)]: {
       margin: "20% auto auto auto",
+      paddingBottom: 10,
     },
-    [theme.breakpoints.down(849)]: {
-      margin: "5% auto auto auto",
-    },
+    // [theme.breakpoints.down(1131)]: {
+    //   margin: "20% auto auto auto",
+    // },
+    // [theme.breakpoints.down(849)]: {
+    //   margin: "5% auto auto auto",
+    // },
 
-    [theme.breakpoints.down(721)]: {
-      margin: "auto",
-    },
+    // [theme.breakpoints.down(721)]: {
+    //   margin: "auto",
+    // },
     [theme.breakpoints.down(600)]: {
-      paddingBottom: 0,
+      margin: "auto auto auto",
     },
   },
   error404TextYellow: {
     color: theme.palette.color.yellow,
     [theme.breakpoints.down(415)]: {
-      fontSize: 16
+      fontSize: 16,
     },
   },
-  logo: {
+  dogImage: {
     width: "100%",
+    // height: "80%"
   },
 });
 
