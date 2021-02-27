987670fefd1019fcc173201ca0878de24ac7184b
CornetS28 <corsam28@gmail.com>
2021-02-27 13:34:19 -0500

    Add validators fot the login form

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -17,3 +17,31 @@ export const contactUsSchema = yup.object().shape({
     .min(5, "Message must contain at least 5 characters")
     .required("Message is required"),
 });
+
+export const signupSchema = yup.object().shape({
+  first_name: yup.string().required("First name is required"),
+  last_name: yup.string().required("Last name is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must contain at least 6 characters")
+    .required("Password is required"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf(
+      [yup.ref("password"), null],
+      "Password confirmation doesn't match the password"
+    )
+    .required("Confirm password is required"),
+});
+
+export const signinSchema = yup.object().shape({
+  usernameOrEmail: yup.string().required("Username or Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must contain at least 6 characters")
+    .required("Password is required"),
+});
