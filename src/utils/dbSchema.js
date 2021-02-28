22a8f3f26ac9b40ec819858c0b6f1b20ac9c509b
CornetS28 <corsam28@gmail.com>
2021-02-28 17:21:57 -0500

    Minor fix

--- src/utils/dbSchema.js
+++ src/utils/dbSchema.js
@@ -191,7 +191,7 @@ const db = {
     {
       comId: "7hgsd566j5h343432332565gf46",
       image: CommunityImg,
-      title: "Hope",
+      title: "Busyness",
       briefDescription:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.",
       totalDescription:
